c046ae246534a5e620f1ff69c101e6c78c7000a5
MoE9120 <MPRE222@more2learn.de>
2024-12-17 12:50:43 +0100

    BMI-Rechner

---
+++ src/Thema1_Grundlage/BMI_Rechner.java
@@ -0,0 +1,23 @@
+package Thema1_Grundlage;
+
+import utils.IO;
+
+public class BMI_Rechner {
+
+	public static void main(String[] args) {
+
+		System.out.println("BMI-Rechner");
+
+		String name = IO.promptAndReadString("Bitte Namen eingeben: ");
+
+		double groesse = IO.promptAndReadDouble("Bitte Größe eingeben: ");
+		;
+		double gewicht = IO.promptAndReadDouble("Bitte Gewicht eingeben: ");
+		;
+		double bmi = gewicht / (groesse * groesse);
+		double gerunderterBmi = IO.round(bmi * 100.0, 0) / 100.0;
+		IO.writeln(name + ", dein BMI beträgt " + gerunderterBmi);
+
+	}
+
+}
